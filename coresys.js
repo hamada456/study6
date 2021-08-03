394e284b9c174b5eb397b94bdce41585c30d48d3
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-08-03 11:03:25 +0900

    btnで画像表示

--- coresys.js
+++ coresys.js
@@ -1,12 +1,17 @@
 
 const $main = document.getElementsByClassName("main-img")[0];
 
+
 //btnsのclassをクリックしたらmain-imgが消える
-document.getElementsByClassName("btns")[0].addEventListener("click",() => {
-console.log("b");
-    if($main.style.display === "") {
+document.getElementsByClassName("btns")[0].addEventListener("click",() =>{
+    //if($main.style.display === "") {
         $main.style.display = "none";
-        console.log("c");
-    }
+    //}
 });
 
+//ボタンクリックで画像表示
+document.getElementById("A").addEventListener("click",() =>{
+    if(document.getElementsByClassName("product-imgs")[0].style.display === "") {
+        document.getElementsByClassName("product-imgs")[0].style.display = "block";
+    }
+});
