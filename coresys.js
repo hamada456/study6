444fd2daa710c1789ded66916e0e54a8e7f63a09
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-09-07 16:52:24 +0900

    09071700

--- coresys.js
+++ coresys.js
@@ -32,22 +32,23 @@
 
         let products = [];
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://jsondata.okiba.me/v1/json/Bg8NV210907014508");//./coresys.json
+        xhr.open("GET", "https://jsondata.okiba.me/v1/json/87m8p210907062709");//./coresys.json
         xhr.onload = () => {
             let responseJson = JSON.parse(xhr.response);
-            const products = responseJson.nigiriProducts;
+            products = responseJson;
+            console.log(products);
             console.log(responseJson.products);
-            console.log(responseJson.nigiriProducts);
-            console.log(responseJson.nigiriProducts[0].nigiriSrc);
+            console.log(products[0]);
+            
         }
-        
         xhr.send();
+        let nigiriProducts = products[0]
         
         //９個書き換え
         let productNum = 0;
         while(productNum < $productImgs.length){
             //URLか名前か値段が空なら準備中画像を表示
-            const nNum = nigiriProducts[productNum];
+            let nNum = nigiriProducts[productNum];
             if(nNum.nigiriSrc === ""||nNum.nigiriName === ""||nNum.nigiriPrice === ""){
                 $productImgs[productNum].src = "./images/準備中.png";
                 $productName[productNum].textContent = "準備中";
